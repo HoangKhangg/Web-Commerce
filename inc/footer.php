2337d76be939be9b20f529f6e1112a3a1881e161
NGUYỄN MINH NHỰT <4501104166@student.hcmue.edu.vn>
2022-04-11 23:58:40 +0700

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,139 @@
+<div id="footer-wp" style=" background: #f12a43; color: white;">
+    <div id="foot-body">
+        <div class="wp-inner clearfix">
+            <div class="block" id="info-company">
+            
+                <h3 class="title" style="color: white;">SPACE TEAM</h3>
+                <p class="desc" style="color: white;">SPACE TEAM luôn cung cấp luôn là sản phẩm chính hãng có thông tin rõ ràng, chính sách ưu đãi cực lớn cho khách hàng có thẻ thành viên.</p>
+                <div id="payment">
+                    <div class="thumb">
+                        <img src="public/images/img-foot.png" alt="">
+                    </div>
+                </div>
+            </div>
+            
+            <div class="block menu-ft" id="info-shop">
+                <h3 class="title" style="color: white;">Thông tin cửa hàng</h3>
+                <ul class="list-item">
+                    <li>
+                        <p style="color: white;">351A - Lạc Long Quân Phường 5 Quận 11</p>
+                    </li>
+                    <li>
+                        <p style="color: white;">0377077630-092361253</p>
+                    </li>
+                    <li>
+                        <p style="color: white;">spaceteam.hcmue@gmail.com</p>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="block menu-ft policy" id="info-shop">
+                <h3 class="title" style="color: white;">Chính sách mua hàng</h3>
+                <ul class="list-item">
+                    <li>
+                        <a href="" title="" style="color: white;">Quy định - chính sách</a>
+                    </li>
+                    <li>
+                        <a href="" title="" style="color: white;">Chính sách bảo hành - đổi trả</a>
+                    </li>
+                    <li>
+                        <a href="" title="" style="color: white;">Chính sách hội viện</a>
+                    </li>
+                    <li>
+                        <a href="" title="" style="color: white;">Giao hàng - lắp đặt</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+       
+    </div>
+    <div id="foot-bot">
+        <div class="wp-inner">
+            <p id="copyright" style="color: white;">© SPACE TEAM | Trường Đại Học Sư Phạm TP.Hồ Chí Minh</p>
+        </div>
+    </div>
+</div>
+</div>
+<div id="menu-respon">
+    <a href="?page=home" title="" class="logo">SPACE TEAM</a>
+    <div id="menu-respon-wp">
+        <div id="main-menu-wp">
+            <ul class="" id="main-menu-respon">
+                <li>
+                    <a href="?" title="">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="#" title="">Laptop</a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="?mod=product&act=category_product&id=1" title="">DELL</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=2" title="">ASUS</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=3" title="">HP</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=4" title="">MACBOOK</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=5" title="">LENOVO</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=6" title="">MSI</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=7" title="">ACER</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="?mod=post&act=blog" title="">Blog</a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="?mod=post&act=main&id=1" title="">Đánh giá</a>
+                        </li>
+                        <li>
+                            <a href="?mod=post&act=main&id=2" title="">Cẩm nang</a>
+                        </li>
+                        <li>
+                            <a href="?mod=post&act=main&id=3" title="">Tin tức</a>
+                        </li>
+                        <li>
+                            <a href="?mod=post&act=main&id=4" title="">So sánh</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="?mod=page&act=main" title="">Giới thiệu</a>
+                </li>
+                <li>
+                    <a href="?mod=page&act=contact" title="">Liên hệ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="btn-top"><img src="public/images/icon-to-top.png" alt=""/></div>
+<div id="fb-root"></div>
+<script>(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id))
+            return;
+        js = d.createElement(s);
+        js.id = id;
+//        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.8&appId=849340975164592";
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="198558491069654"
+  theme_color="#13cf13"
+  logged_in_greeting="Xin chào, tôi có thể hổ trợ gì cho bạn không?"
+  logged_out_greeting="Xin chào, tôi có thể hổ trợ gì cho bạn không?">
+</div>
+</body>
+</html>
---
+++ modules/page/contact.php
@@ -0,0 +1,52 @@
+<?php
+get_header()
+?>
+
+<div id="main-content-wp" class="clearfix detail-blog-page">
+    <div class="wp-inner">
+        <div class="secion" id="breadcrumb-wp">
+            <div class="secion-detail">
+                <ul class="list-item clearfix">
+                    <li>
+                        <a href="?" title="">Trang chủ</a>
+                    </li>
+                    <li>
+                        <a href="?" title="">Liên hệ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <?php get_sidebar_product_page(); ?> 
+        
+        <div class="main-content fl-right">
+            <div class="section" id="detail-blog-wp">
+                <div class="section-head clearfix">
+                    <h3 class="section-title">Liên hệ</h3>
+                </div>
+                <div class="section-detail">
+                    <span class="create-date">2020-07-09 11:33:00</span>
+                    <div class="detail">
+                        <p>Website thương mại điện tử SPACETEAM</p>
+                        <p>Địa chỉ: 351A Lạc Long Quân Phường 5 Quận 11 TP.Hồ Chí Minh</p>
+                        <p>Số điện thoại: 0377077630</p>
+                        <p>Email: spaceteam.hcmue@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4661.113152141683!2d106.6427739275321!3d10.771939673442276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ebd9847d7a3%3A0x7c62ba428463a53e!2zMzUxQSBM4bqhYyBMb25nIFF1w6JuLCBQaMaw4budbmcgNSwgUXXhuq1uIDExLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1595169995930!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <div class="section" id="social-wp">
+                <div class="section-detail">
+                    <div class="fb-like" data-href="" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+                    <div class="g-plusone-wp">
+                        <div class="g-plusone" data-size="medium"></div>
+                    </div>
+                    <div class="fb-comments" id="fb-comment" data-href="" data-numposts="5"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
